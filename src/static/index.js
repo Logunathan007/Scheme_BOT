1c879b4f0dfb836f04873a4841dbfe537869b42f
Logunathan J <logulogu20032003@gmail.com>
2024-04-27 15:24:47 +0530

    FET: Initializing User Interface(UI)

---
+++ src/static/index.js
@@ -0,0 +1,13 @@
+const bot_icon = document.querySelector(".bot_icon")
+const msg_box = document.querySelector(".msg_box")
+
+bot_icon.addEventListener("click",()=>{
+    if(msg_box.classList.contains("on")){
+        msg_box.classList.add("off")
+        msg_box.classList.remove("on")
+    }else{
+        msg_box.classList.add("on")
+        msg_box.classList.remove("off")
+    }
+    col
+});
