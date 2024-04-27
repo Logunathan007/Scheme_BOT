54fce1ca08254450bd68da947b1ee20723cca829
Logunathan J <logulogu20032003@gmail.com>
2024-04-27 10:50:40 +0530

    FET: initial program

--- src/main.py
+++ src/main.py
@@ -1,1 +1,26 @@
-import os
+input_str =  "Hi My name is Logunathan J . my age is 21" # input()
+
+input_str = input_str.replace(",",".").replace("and",".")
+
+input_ls = input_str.split(".")
+
+key = ["name","age"]
+
+helper = ["hi","my","is"];
+
+pairs = {}
+
+for j in input_ls:
+    ls = j.split(" ")
+    keystr = ""
+    for i in range(len(ls)):
+        if(ls[i].lower() in helper):
+            ls[i] = ""
+        elif(ls[i].lower() in key):
+            keystr+=ls[i]+" ";
+            ls[i] = ""
+    pairs[keystr] = " ".join(ls).strip();
+print(pairs)
+
+            
+
